a971244cb76e562e0eeb9d05d7cd7e1d5ae50c2b
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-07-29 20:58:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>Flora - Skincare Products Ecommerce Website Template</title>
+  <title>Glory Vogue USA</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
